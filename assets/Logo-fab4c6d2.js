e2cbddcfd7382040136b979f5e5827b4f7923bef
omr-muhammad <omr-muhammad@users.noreply.github.com>
2024-05-02 15:34:47 +0000

    deploy: c46973a11df81a15a76a8b280149ff1ca640b918

---
+++ assets/Logo-fab4c6d2.js
@@ -0,0 +1,1 @@
+import{j as o,L as s}from"./index-2f12b851.js";const l="_logo_7b2ls_1",t={logo:l};function n(){return o.jsx(s,{to:"/react-training-world-wise/",children:o.jsx("img",{src:"/logo.png",alt:"WorldWise logo",className:t.logo})})}export{n as L};
--- assets/PageNotFound-afea9a30.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-1a5a69c7.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
