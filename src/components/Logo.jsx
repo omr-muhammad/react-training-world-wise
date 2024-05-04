df3185bd55dadda9353ee4423636ad72f74f5368
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-05-04 06:32:24 +0300

    fixed logo image url

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,12 @@
 import { Link } from 'react-router-dom';
 import styles from './Logo.module.css';
 
+const imgUrl = new URL('./logo.png', import.meta.url).href;
+
 function Logo() {
   return (
     <Link to='/react-training-world-wise/'>
-      <img src='/logo.png' alt='WorldWise logo' className={styles.logo} />
+      <img src={imgUrl} alt='WorldWise logo' className={styles.logo} />
     </Link>
   );
 }
