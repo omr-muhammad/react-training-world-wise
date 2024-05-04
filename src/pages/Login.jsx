59d12c77206d6711df8016f4fdd1952b683dca03
omar-muhammad <omarelsayedmuhammad@gmail.com>
2024-05-04 06:39:14 +0300

    added onClick to login button to handle navigation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,45 @@
-import { useState } from "react";
-import styles from "./Login.module.css";
-import Button from "../components/Button";
+import { useState } from 'react';
+import styles from './Login.module.css';
+import Button from '../components/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
-  const [password, setPassword] = useState("qwerty");
+  const [email, setEmail] = useState('jack@example.com');
+  const [password, setPassword] = useState('qwerty');
+  const navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/react-training-world-wise/app');
+  }
 
   return (
     <main className={styles.login}>
       <form className={styles.form}>
         <div className={styles.row}>
-          <label htmlFor="email">Email address</label>
+          <label htmlFor='email'>Email address</label>
           <input
-            type="email"
-            id="email"
+            type='email'
+            id='email'
             onChange={(e) => setEmail(e.target.value)}
             value={email}
           />
         </div>
 
         <div className={styles.row}>
-          <label htmlFor="password">Password</label>
+          <label htmlFor='password'>Password</label>
           <input
-            type="password"
-            id="password"
+            type='password'
+            id='password'
             onChange={(e) => setPassword(e.target.value)}
             value={password}
           />
         </div>
 
         <div>
-          <Button type="primary">Login</Button>
+          <Button onClick={handleClick} type='primary'>
+            Login
+          </Button>
         </div>
       </form>
     </main>
